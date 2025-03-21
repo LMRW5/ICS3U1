f7a6e003a3039b809906ad363c2170fe7c733803
LMRW5 <153659669+LMRW5@users.noreply.github.com>
2025-03-21 09:26:53 -0400

    Update tictactoe.java
    added more helper functions and minimax function

--- main/src/tictactoe.java
+++ main/src/tictactoe.java
@@ -1,41 +1,40 @@
 import java.util.*;
 
 public class tictactoe {
-    public static String turn(String[][] grid){
+    public static String turn(String[][] grid) {
         // finds whos player turn
         int xctr = 0;
         int octr = 0;
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j<3;j++){
-                int ctr = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 String val = grid[i][j];
-                if (val.equals("X")){
+                if (val.equals("X")) {
                     xctr += 1;
-                }
-                else if (val.equals("O")){
+                } else if (val.equals("O")) {
                     octr += 1;
                 }
             }
         }
-        if (xctr > octr){
+        if (xctr > octr) {
             return "O";
-        }
-        else{
+        } else {
             return "X";
         }
     }
-    static String[][] update(String[][] grid, int xpair, int ypair){
-        //returns updated grid
+
+    static String[][] update(String[][] grid, int xpair, int ypair) {
+        // returns updated grid
         String turn = turn(grid);
-        grid[ypair][xpair] = turn;
-        return grid;
+        String[][] newgrid = grid.clone();
+        newgrid[ypair][xpair] = turn;
+        return newgrid;
     }
 
-    static Boolean terminal(String[][] grid){
+    static Boolean terminal(String[][] grid) {
         // checks if game has ended
-        for (int i = 0; i < 3; i++){
-            for (int j = 0; j<3; j++){
-                if (grid[i][j].equals("E")){
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (grid[i][j].equals("E")) {
                     return false;
                 }
             }
@@ -43,26 +42,26 @@ public class tictactoe {
         return true;
     }
 
-    static String winner(String[][] grid){
+    static String winner(String[][] grid) {
         // returns winner
 
         // horizontal
-        for (int i = 0; i<3;i++){
-            if (grid[i][0].equals(grid[i][1]) & grid[i][1].equals(grid[i][2])){
+        for (int i = 0; i < 3; i++) {
+            if (grid[i][0].equals(grid[i][1]) & grid[i][1].equals(grid[i][2])) {
                 return grid[i][0];
             }
         }
         // vertical
-        for (int i = 0; i < 3; i++){
-            if (grid[0][i].equals(grid[1][i]) & grid[1][i].equals(grid[2][i])){
+        for (int i = 0; i < 3; i++) {
+            if (grid[0][i].equals(grid[1][i]) & grid[1][i].equals(grid[2][i])) {
                 return grid[0][i];
             }
         }
         // diagnoals
-        if (grid[0][0].equals(grid[1][1]) & grid[1][1].equals(grid[2][2]) & !(grid[0][0].equals("E"))){
+        if (grid[0][0].equals(grid[1][1]) & grid[1][1].equals(grid[2][2]) & !(grid[0][0].equals("E"))) {
             return grid[0][0];
         }
-        if (grid[0][2].equals(grid[1][1]) & grid[1][1].equals(grid[2][0]) & !(grid[0][0].equals("E"))){
+        if (grid[0][2].equals(grid[1][1]) & grid[1][1].equals(grid[2][0]) & !(grid[0][2].equals("E"))) {
             return grid[1][1];
         }
 
@@ -70,48 +69,106 @@ public class tictactoe {
         return null;
 
     }
-    
-    static int utility(String[][] grid){
+
+    static int utility(String[][] grid) {
         // returns status of board
         String val = winner(grid);
-        if (val.equals("X")){
+        if (val == null) {
+            return 0;
+        }
+        if (val.equals("X")) {
             return 1;
-        }
-        else if (val.equals("O")){
+        } else if (val.equals("O")) {
             return -1;
-        }
-        else{
+        } else {
             return 0;
         }
     }
-    static int[][] actions(String [][] grid){
-        ArrayList<Array<Integer>> rval = new ArrayList<Array<Integer>>();
-        for (int i = 0; i < grid.length; i++){
-            for (int j = 0; j<grid[0].length;j++){
-                if (grid[i][j].equals("E")){
-                    rval.add([i,j]);
+
+    static List<int[]> actions(String[][] grid) {
+        List<int[]> rval = new ArrayList<>();
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j].equals("E")) {
+                    rval.add(new int[] { i, j });
                 }
             }
         }
+        return rval;
 
     }
-    static String[] minimax(String[][] grid){
+
+    static String[][] result(String[][] board, int[] action) {
+        int i = action[0];
+        int j = action[1];
+        String[][] newboard = board.clone();
+        newboard[i][j] = turn(board);
+        return newboard;
+    }
+
+    static int[] minimax(String[][] grid) {
         // returns best move for the grid
         String turn = turn(grid);
-        if (terminal(grid)){
+        if (terminal(grid)) {
             return null;
         }
-        if (turn.equals("X")){
-            // MAX  player
-        }
-        else if (turn.equals("O")){
+        if (turn.equals("X")) {
+            // MAX player
+            double best = -999999999;
+            int[] bestAction = new int[2];
+            for (int[] action : actions(grid)) {
+                double value = minval(result(grid, action));
+                if (value > best) {
+                    best = value;
+                    bestAction = action;
+                }
+            }
+            return bestAction;
+        } else {
             // MIN player
+            double best = 999999999;
+            int[] bestAction = new int[2];
+            for (int[] action : actions(grid)) {
+                double value = maxval(result(grid, action));
+                if (value < best) {
+                    best = value;
+                    bestAction = action;
+                }
+
+            }
+            return bestAction;
         }
 
-        static maxi
     }
+
+    public static double maxval(String[][] state) {
+        if (terminal(state)) {
+            return utility(state);
+        }
+        double value = -999999999;
+        for (int[] action : actions(state)) {
+            if (value > minval(result(state, action))) {
+                value = minval(result(state, action));
+            }
+        }
+        return value;
+    }
+
+    public static double minval(String[][] state) {
+        if (terminal(state)) {
+            return utility(state);
+        }
+        double value = 999999999;
+        for (int[] action : actions(state)) {
+            if (value < maxval(result(state, action))) {
+                value = maxval(result(state, action));
+            }
+        }
+        return value;
+    }
+
     public static void main(String[] args) {
-        String[][] grid = {{"E","E","E"},{"E","E","E"},{"E","E","E"}};
+        String[][] grid = { { "E", "E", "E" }, { "E", "E", "E" }, { "E", "E", "E" } };
 
     }
 }
