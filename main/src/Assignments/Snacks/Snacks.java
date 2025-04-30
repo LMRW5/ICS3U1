c6ed91273aaa67298790705b244e664a33f29b8c
Wu <Ryan.Wu5@student.tdsb.on.ca>
2025-04-30 14:45:42 -0400

    autocommit

--- main/src/Assignments/Snacks/Snacks.java
+++ main/src/Assignments/Snacks/Snacks.java
@@ -11,6 +11,9 @@ public class Snacks {
         this.sugar = sugar;
         this.name = name;
     }
+    /**
+     * default constructor for the snacks class, sets all values to 0.0 and name to "unnamed"
+     */
     public Snacks(){
         this(0.0,0.0,"unnamed");
     }
@@ -23,9 +26,17 @@ public class Snacks {
     public String getName(){
         return this.name;
     }
+    /**
+     * returns the percentage of sugar in the snack
+     * @return
+     */
     public double percentageSugar(){
         return (this.sugar/this.calories) * 100;
     }
+    /**
+     * prints out if the snack is over or under the recommended amount of sugar, if it is under it shows how much more sugar you can consume
+     */
+    // 36 grams of sugar is the recommended amount of sugar to consume in a day
     public void sugarRequirements(){
         if (this.sugar < 36){
             System.out.printf("after eating this, it is recommended to only consume %d more grams of sugar\n", 36-this.sugar);
@@ -37,6 +48,11 @@ public class Snacks {
             System.out.println("this is just enough sugar to consume today");
         }
     }
+    /**
+     * prints out if the snack is over or under the recommended amount of calories, if it is under it shows how much more calories you can consume
+     */
+    // 3000 calories is the recommended amount of calories to consume in a day
+    
     public void calorieRequirements(){
         if (this.calories < 3000){
             System.out.printf("after eating this, it is recommended to only eat %d more calories\n", 3000-this.calories);
